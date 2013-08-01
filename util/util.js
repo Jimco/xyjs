aa5561f68b9a4d79bbce382a3695358b9449370c
Xie Jiancong <zqianduan@gmail.com>
2013-08-01 19:34:49 +0800

    增加 uniqueId 函数，生成自定义长度的 id

--- util/util.js
+++ util/util.js
@@ -489,6 +489,17 @@
       output = XY.Util.utf8Decode(output);
 
       return output;
+    },
+
+    uniqueId: function(length){
+      var text = ''
+        , length = length || 6
+        , possible = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+      for(var i = 0; i < length; i++){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
     }
 
   };
