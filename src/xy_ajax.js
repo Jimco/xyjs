dc8c4745f0f54872b9423f7ed42ce510f96c4063
Jiancong Xie <jimco@color7.net>
2013-08-06 17:18:26 +0800

    ajax、animate 模块

---
+++ src/xy_ajax.js
@@ -0,0 +1,7 @@
+/**
+ * xy Ajax模块(依赖 xy_core.js)
+ * Author: xjiancong@gmail.com
+ */
+XY.install('Ajax', function(XY){
+
+});
---
+++ src/xy_animate.js
@@ -0,0 +1,7 @@
+/**
+ * xy Anim模块(依赖 xy_core.js)
+ * Author: xjiancong@gmail.com
+ */
+XY.install('Anim', function(XY){
+
+});
