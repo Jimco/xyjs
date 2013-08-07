9de4b0ddb0c2a969ab443ca002d0f3083b58ca73
Jiancong Xie <jimco@color7.net>
2013-08-07 11:47:40 +0800

    增加常用方法

--- src/xy_core.js
+++ src/xy_core.js
@@ -35,17 +35,62 @@
     version: '0.0.1',
 
     /**
-     * 遍历对象
+     * 遍历对象并执行回调
+     * @param  {Object}   obj     待遍历对象
+     * @param  {Function} fn      回调函数
+     * @param  {Object}   context 上下文
+     * @return {Object}           返回遍历的对象
      */
-    each: function(object, fn){
-      if(object){
-        for(var i in object){
-          if(i !== 'length' && i !=='item'){
-            fn.call(me, object[i], i);
+    each: function(obj, fn, context){
+      var isObj = obj.length === undefined || typeof obj === 'function'
+        , i;      
+      
+      if( isObj ){
+        for( i in obj ){
+          if( fn.call(context, i, obj[i]) === false ){
+            break;
           }
         }
       }
-      return object;
+      
+      return obj;
+    },
+
+    /*
+     * 将对象转换成真实数组
+     * 常用于将arguments, NodeList等array-like对象转换成真实数组
+     * @param {} source 任意类型的数据
+     * @param {Array} target 目标数组
+     * @return {Array} 真实的数组
+     */
+    makeArray: function(source, target){
+      target = target || [];
+      var i = 0
+        , len = source.length;
+
+      if( source !== null && source !== undefined ){
+        if( XY.isArray(source) && XY.isArray(target) && !target.length ){
+          return source;
+        } 
+        
+        if( typeof len !== 'number' || 
+          typeof source === 'string' || 
+          XY.isFunction(source) || 
+          XY.isRegExp(source) || 
+          source === window ||
+          // select元素有length属性，select[0]将直接返回第一个option
+          // form元素也有length属性
+          source.tagName && rSelectForm.test(source.tagName) ){
+            target[ target.length++ ] = source;
+        }
+        else{
+          for( ; i < len; i++ ){
+            target[ target.length++ ] = source[i];
+          }
+        }
+      }
+      
+      return target;
     },
 
     /**
@@ -88,6 +133,10 @@
 
     isFunction: function(obj){
       return __isType(obj, 'Function');
+    },
+
+    isRegExp: function(obj){
+      return __isType(obj, 'RegExp');
     },
 
     isUndefined: function(obj){
@@ -212,6 +261,112 @@
         text += possible.charAt(Math.floor(Math.random() * possible.length));
       }
       return text;
+    },
+
+    parseXML: function( data ) {
+      var xml, tmp;
+      try {
+        // 标准浏览器
+        if ( window.DOMParser ) { 
+          tmp = new DOMParser();
+          xml = tmp.parseFromString( data , 'text/xml' );
+        }
+        // IE6/7/8
+        else{
+          xml = new ActiveXObject( 'Microsoft.XMLDOM' );
+          xml.async = 'false';
+          xml.loadXML( data );
+        }
+      } catch( e ) {
+        xml = undefined;
+      }
+      
+      return xml;
+    },
+    
+    encodeJSON: function( o ) {
+      if ( typeof o === 'string' ){
+        return window.JSON ? JSON.parse( o ) : eval( "(" + o + ')' );
+      }else{
+        return o ;
+      }
+    },
+
+    decodeJSON: function (json) {
+      if (window.JSON && window.JSON.stringify) {
+        return JSON.stringify(json);
+      }
+      var html = [];
+      if(typeof json == 'object') {
+        if(json instanceof Array){
+          var ar = [];
+          html.push("[");
+          for(var i = 0; i < json.length; i ++) {
+            ar.push(this.decodeJSON(json[i]));
+          }
+          html.push(ar.join());
+          html.push("]");
+        } else {
+          html.push("{");
+          var ar = [];
+          for(var p in json) {
+            ar.push("\"" + p + "\":" + (this.decodeJSON(json[p])));
+          }
+          html.push(ar.join());
+          html.push("}");
+        }
+        return html.join("");
+      } else {
+        if(typeof json !== 'number') {
+          return "\"" + ( json || "" ) + "\"";
+        } else {
+          return json;
+        }
+      }
+    },
+
+    encodeHTML: function(text){
+      return String(text).replace(/["<>& ]/g, function(all){
+        return "&" + {
+            '"': 'quot',
+            '<': 'lt',
+            '>': 'gt',
+            '&': 'amp',
+            ' ': 'nbsp'
+        }[all] + ";";
+      });
+    },
+
+    isEmptyObject: function( obj ){
+      var name;
+      for( name in obj ){
+        return false;
+      }
+      return true;
+    },
+
+    setCookie: function(name, value, time){
+      var date = new Date();
+      time = time || 7;
+      date.setTime(date.getTime() + time*24*60*60*1000);
+      document.cookie = name + "=" + value + "; expires=" + date.toGMTString()+"; path=/";
+    },
+
+    getCookie: function(name){
+      var search = name + '=';
+      if(document.cookie.length > 0){ 
+        offset = document.cookie.indexOf(search); 
+          if(offset != -1){ 
+            offset += search.length; 
+            end = document.cookie.indexOf(';',offset); 
+            if(end == -1) end = document.cookie.length;
+            return document.cookie.substring(offset, end); 
+          }else{
+            return ''; 
+          }
+      }else{
+        return '';
+      }
     },
 
     /**
