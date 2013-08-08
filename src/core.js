0e3912367f2e75bafe14eb7be119f125f86ee09c
Jiancong Xie <jimco@color7.net>
2013-08-08 17:12:33 +0800

    增加简单模版方法

--- src/core.js
+++ src/core.js
@@ -8,6 +8,7 @@
   var me = this
     , EMPTY = ''
     , __eventTarget = {}
+    , __tplCache = {}
     , __isType = function(obj, type){
         return Object.prototype.toString.call(obj) === '[object '+ type +']';
       };
@@ -421,6 +422,25 @@
 
     delCookie: function(name, path, domain){
       XY.setCookie( name, '', 'Thu, 01 Jan 1970 00:00:00 GMT', path, domain );
+    },
+
+    template: function(str, data){
+      var fn = !/\W/.test(str) ? tplCache[str] = tplCache[str] ||
+        XY.template(document.getElementById(str).innerHTML) :
+        new Function("obj",
+          "var p=[],print=function(){p.push.apply(p,arguments);};" +
+          "with(obj){p.push('" +
+          str
+            .replace(/[\r\t\n]/g, " ")
+            .split("<%").join("\t")
+            .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+            .replace(/\t=(.*?)%>/g, "',$1,'")
+            .split("\t").join("');")
+            .split("%>").join("p.push('")
+            .split("\r").join("\\'")
+        + "');}return p.join('');");
+      
+      return data ? fn( data ) : fn;
     },
 
     /**
