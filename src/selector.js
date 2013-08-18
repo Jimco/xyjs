e5584942adfc23df3db997814bc2d2410723bbd5
Xie Jiancong <zqianduan@gmail.com>
2013-08-18 22:19:39 +0800

    选择器相关接口

---
+++ src/selector.js
@@ -0,0 +1,24 @@
+/**
+ * 选择器模块
+ * Author: xjiancong@gmail.com
+ * Date: 2013-08-18
+ */
+(function(window, XY, undefined){
+
+  var xySelector = {
+      
+    }
+
+
+  XY.mix(XY, {
+    // DOM 元素过滤器
+    filter: function(source, selector){
+
+    },
+
+    query: function(selector, context){
+
+    }
+  });
+
+})(window, window.XY || {});
