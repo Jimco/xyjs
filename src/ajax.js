e998d84505de33e08155d98bb27d6f500a28fc88
Jiancong Xie <jimco@color7.net>
2013-08-29 16:16:00 +0800

    简单ajax封�

--- src/ajax.js
+++ src/ajax.js
@@ -2,6 +2,56 @@
  * xy Ajax模块(依赖 xy_core.js)
  * Author: xjiancong@gmail.com
  */
-XY.install('Ajax', function(XY){
+(function(window, XY){
 
-});
+  function xyAjax(url, callbackFn){
+
+    this.bindFn = function(caller, object){
+      return function(){
+        return caller.apply(object, [object])
+      }
+    }
+
+    this.stateChange = function(object){
+      if(this.request.readyState == 4){
+        this.callbackFn(this.request.responseText);
+      }
+    }
+
+    this.getRequest = function(){
+      if (window.ActiveXObject){
+        return new ActiveXObject('Microsoft.XMLHTTP');
+      }else if(window.XMLHttpRequest){
+        return new XMLHttpRequest();
+      }
+      return false;
+    }
+
+    this.postBody = (arguments[2] || '');
+
+    this.callbackFn = callbackFn;
+    this.url = url;
+    this.request = this.getRequest();
+    
+    if(this.request) {
+      var req = this.request;
+      req.onreadystatechange = this.bindFn(this.stateChange, this);
+
+      if (this.postBody !== '') {
+        req.open('POST', url, true);
+        req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        req.setRequestHeader('Connection', 'close');
+      } else {
+        req.open("GET", url, true);
+      }
+
+      req.send(this.postBody);
+    }
+  }
+
+  XY.mix(XY, {
+    ajax: xyAjax
+  });
+
+})(window, window.XY || {}); 
