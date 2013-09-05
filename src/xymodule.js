7004444e2d49b3c21f30dca6985ba894ee13d41b
Jiancong Xie <jimco@color7.net>
2013-09-05 12:04:06 +0800

    模块管理变量及接口定义

--- src/xymodule.js
+++ src/xymodule.js
@@ -3,7 +3,38 @@
  * Date: 2013-09-04
  */
 (function(window, XY, undefined){
-  
+  var moduleCache = {}      // 模块加载时的队列数据存储对象
+    , modifyCache = {}      // modify的临时数据存储对象
+    , moduleOptions = {     // 模块加载器的配置对象
+      baseUrl: null,
+      charset: {}
+    }
+    , head = document.head || 
+      document.getElementsByTagName( 'head' )[0] || 
+      document.documentElement;
+
+
+  XY.mix(XY, {
+    module: {},
+
+    use: function(){
+
+    },
+
+    config: function(options){
+      var baseUrl = options.baseUrl
+        , isHttp = baseUrl.slice(0, 4) === 'http';
+
+      if(isHttp){
+        moduleOptions.baseUrl = baseUrl;
+      }
+    }
+  });
+
+
+  window.define = function(name, deps, factory){
+
+  }
 
   
 })(window, window.XY ||{});
