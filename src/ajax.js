733d5f086cbb8bc3d83363b8f69b63e778d1b9d9
Jiancong Xie <jimco@color7.net>
2013-09-16 17:22:18 +0800

    createXHR更新

--- src/ajax.js
+++ src/ajax.js
@@ -9,14 +9,36 @@
       url: '/',
       async: false,
       data: null,
-      success: function(e){ }
+      success: function(e){},
+      failure: function(e){}
+    }
+    , defaultHeaders = {
+      contentType: 'application/x-www-form-urlencoded',
+      requestedWith: 'XMLHttpRequest',
+      accept: {
+        '*': 'text/javascript, text/html, application/xml, text/xml, */*',
+        xml: 'application/xml, text/xml',
+        html: 'text/html',
+        json: 'application/json, text/javascript',
+        js: 'application/javascript, text/javascript'
+      }
     };
 
   var xyAjax = {
-      createXHR: function(){
-        if('ActiveXObject' in window){
-          if('XMLHttpRequest' in window) return XMLHttpRequest();
-          
+      createXHR: function(o){
+        if(o.crossOrigin === true){
+          var xhr = 'XMLHttpRequest' in window ? new XMLHttpRequest() : null;
+          if(xhr && 'withCredentials' in xhr){
+            return xhr;
+          }
+          else if('XDomainRequest' in window){
+            return new XDomainRequest();
+          }
+          else{
+            throw new Error('Browser does not support cross-origin requests');
+          }
+        }
+        else if('ActiveXObject' in window){
           try{
             return new ActiveXObject( 'Microsoft.XMLHTTP' );
           }
