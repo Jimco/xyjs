20433d5eab8f430239f39c5f42cdc68f6b5a1848
Jiancong Xie <jimco@color7.net>
2014-03-06 16:24:59 +0800

    ajax module modify

--- src/ajax.js
+++ src/ajax.js
@@ -4,73 +4,98 @@
  */
 (function(window, XY){
 
-  var defaultConfig = {
-      method: 'POST',
-      url: '/',
-      async: false,
-      data: null,
-      success: function(e){},
-      failure: function(e){}
-    }
-    , defaultHeaders = {
-      contentType: 'application/x-www-form-urlencoded',
-      requestedWith: 'XMLHttpRequest',
-      accept: {
-        '*': 'text/javascript, text/html, application/xml, text/xml, */*',
-        xml: 'application/xml, text/xml',
-        html: 'text/html',
-        json: 'application/json, text/javascript',
-        js: 'application/javascript, text/javascript'
-      }
-    };
+  function Ajax(options){
+    this.options = options;
+    this._initialize();
+  }
 
-  var xyAjax = {
-      createXHR: function(o){
-        if(o.crossOrigin === true){
-          var xhr = 'XMLHttpRequest' in window ? new XMLHttpRequest() : null;
-          if(xhr && 'withCredentials' in xhr){
-            return xhr;
+  XY.mix(Ajax, {
+    //请求状态
+    STATE_INIT: 0,
+    STATE_REQUEST: 1,
+    STATE_SUCCESS: 2,
+    STATE_ERROR: 3,
+    STATE_TIMEOUT: 4,
+    STATE_CANCEL:5,
+    
+    // defaultHeaders: 默认 requestHeader 信息
+    defaultHeaders: {
+      'Content-type': 'application/x-www-form-urlencoded UTF-8', // 最常用配置
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+
+    EVENTS: ['succeed', 'error', 'cancel', 'timeout', 'complete'],
+
+    // IE 下 XMLHttpRequest 的版本
+    XHRVersions: ['Microsoft.XMLHTTP'], // 应对IE6与ES5.0beta的错误，仅用最原始版xmlhttp。['MSXML2.XMLHttp.6.0', 'MSXML2.XMLHttp.3.0', 'MSXML2.XMLHttp.5.0', 'MSXML2.XMLHttp.4.0', 'Msxml2.XMLHTTP', 'MSXML.XMLHttp', 'Microsoft.XMLHTTP'], 
+  
+    // getXHR(): 得到一个 XMLHttpRequest 对象
+    // @returns {XMLHttpRequest} : 返回一个 XMLHttpRequest 对象
+    getXHR: function(){
+      var version = Ajax.XHRVersions;
+      if(window.ActiveXObject){
+        while(version.length > 0){
+          try{
+            return new ActiveXObject(version[0]);
           }
-          else if('XDomainRequest' in window){
-            return new XDomainRequest();
+          catch(e){
+            version.shift();
+          }
+        }
+      }
+
+      if(window.XMLHttpRequest){
+        return new XMLHttpRequest();
+      }
+      return null;
+    },
+
+    request: function(url, data, callback, method){
+      if(url.constructor == Object){
+        var a = new Ajax(url);
+      }
+      else{
+        if(typeof data === 'function'){
+          method = callback;
+          callback = data;
+          if(url && url.tagName == 'FORM'){
+            method = method || url.method;
+            data = url;
+            url = url.action;
           }
           else{
-            throw new Error('Browser does not support cross-origin requests');
+            data = '';
           }
         }
-        else if('ActiveXObject' in window){
-          try{
-            return new ActiveXObject( 'Microsoft.XMLHTTP' );
+
+        a = new Ajax({
+          url: url,
+          method: method,
+          data: data,
+          onsucceed: function(){
+            callback.call(this, this.requester.responseText);
           }
-          catch(_){}
-        }
-        else{
-          return new XMLHttpRequest();
-        }
-      },
-
-      handleReadyState: function(){
-
-      },
-
-      setHeaders: function(){
-
-      },
-
-      getRequest: function(){
-
+        })
       }
 
+      a.send();
+      return a;
+    },
+
+    get: function(url, data, callback){
+
+    },
+
+    post: function(url, data, callback){
+
     }
 
-  XY.mix(XY, {
-    ajax: function(options){
-      if(!options.url) return;
-
-
-    }
-
-    
   });
+
+  XY.mix(Ajax.prototype, {
+
+  });
+
+  
 
 })(window, window.XY || {}); 
