bf881c211a27def1934f2d7e5085ad215fb8cdfb
Jimco <zqianduan@gmail.com>
2014-06-11 16:14:30 +0800

    fix base64 error

--- utils/util.js
+++ utils/util.js
@@ -438,7 +438,7 @@
         , chr1, chr2, chr3, enc1, enc2, enc3, enc4
         , i = 0;
 
-      input = window.utf8Encode(input);
+      input = XY.Util.utf8Encode(input);
 
       while (i < input.length){
         chr1 = input.charCodeAt(i++);
@@ -489,6 +489,53 @@
       output = XY.Util.utf8Decode(output);
 
       return output;
+    },
+
+    utf8Encode: function(string){
+      string = string.replace(/\r\n/g, '\n');  
+      var utftext = '';  
+      for (var n = 0; n < string.length; n++) {  
+        var c = string.charCodeAt(n);  
+        if (c < 128) {
+          utftext += String.fromCharCode(c);  
+        } 
+        else if((c > 127) && (c < 2048)) {  
+          utftext += String.fromCharCode((c >> 6) | 192);  
+          utftext += String.fromCharCode((c & 63) | 128);  
+        } 
+        else {  
+          utftext += String.fromCharCode((c >> 12) | 224);  
+          utftext += String.fromCharCode(((c >> 6) & 63) | 128);  
+          utftext += String.fromCharCode((c & 63) | 128);  
+        }  
+ 
+      }  
+      return utftext; 
+    },
+
+    utf8Decode: function(utftext){
+      var string = '';  
+      var i = 0;  
+      var c = c1 = c2 = 0;  
+      while ( i < utftext.length ) {  
+        c = utftext.charCodeAt(i);  
+        if (c < 128) {  
+          string += String.fromCharCode(c);  
+          i++;  
+        } 
+        else if((c > 191) && (c < 224)) {  
+          c2 = utftext.charCodeAt(i+1);  
+          string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));  
+          i += 2;  
+        } 
+        else {  
+          c2 = utftext.charCodeAt(i+1);  
+          c3 = utftext.charCodeAt(i+2);  
+          string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));  
+          i += 3;
+        }  
+      }  
+      return string;
     },
 
     uniqueId: function(length){
